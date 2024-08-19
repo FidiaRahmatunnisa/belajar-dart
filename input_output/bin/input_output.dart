4bc3eafb2e7e29e4e6def57d2045d566252745dc
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2024-08-19 22:59:20 +0700

    operator logika dan relasi

--- input_output/bin/input_output.dart
+++ input_output/bin/input_output.dart
@@ -1,41 +1,45 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-
-  
+  //MATERI OPERATOR LOGIKA
+  bool result;
+  result = (9 != 9) || (2<5) || (9>10); // true
+  result = (9 != 9) || (2<5) && (9>10); // false
+  result = (9 != 9) || ((2<5) && (9<10)); // false
+  print(result);
 
   //MATERI OPERATOR MATEMATIKA
   // cara lainnya
-  int num6 = 5;
-  num6 += 10; // sama dengan num6 = num6 + 10;
-  print(num6);
+  // int num6 = 5;
+  // num6 += 10; // sama dengan num6 = num6 + 10;
+  // print(num6);
 
-  // materi prefix
-  int num5 = 5;
-  --num5;
-  print(num5);
-  ++num5;
-  print(num5);
+  // // materi prefix
+  // int num5 = 5;
+  // --num5;
+  // print(num5);
+  // ++num5;
+  // print(num5);
 
-  // materi postfix
-  int num4 = 7;
-  num4--; // increment
-  print(num4);
-  num4++; // decrement
-  print(num4);
+  // // materi postfix
+  // int num4 = 7;
+  // num4--; // increment
+  // print(num4);
+  // num4++; // decrement
+  // print(num4);
 
-  // modulo
-  int num3 = 7 % 4;
-  print(num3);
+  // // modulo
+  // int num3 = 7 % 4;
+  // print(num3);
 
-  //pembagian cara 2
-  int num2 = 6 ~/ 2; // tidak bisa menggunakan tipe data double
-  print(num2);
+  // //pembagian cara 2
+  // int num2 = 6 ~/ 2; // tidak bisa menggunakan tipe data double
+  // print(num2);
 
-  //pembagian cara 1
-  double num;
-  num = 5 / 2;
-  print(num);
+  // //pembagian cara 1
+  // double num;
+  // num = 5 / 2;
+  // print(num);
 
   // // materi input ouput string type nullable
   // String? input;
