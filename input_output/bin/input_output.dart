6b8b78f226eb443768d1a0d1125a2ce93da1b4c4
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2024-08-19 22:07:38 +0700

    mempelajari input output string dart

--- input_output/bin/input_output.dart
+++ input_output/bin/input_output.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('hello fidia');
+  String? input;
+
+  print('masukan nama anda ? ');
+  input = stdin.readLineSync();
+  print('nama user:');
+  print(input);
+  // print('hello fidia');
 }
