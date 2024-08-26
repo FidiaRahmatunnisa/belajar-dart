ed0a2549aa275d2662909e331d1884faad2aa790
Fidia Rahmatunnisa <fidiarahmatunnisa274@gmail.com>
2024-08-26 11:39:25 +0700

    mempelajari dart dasar materi variabel

---
+++ dart_demo/dart_demo/bin/dart_demo.dart
@@ -0,0 +1,5 @@
+void main(List<String> arguments) {
+  var anything = 12;
+  // anything = 'halo'; //--> tidak bisa, karena saat assign sebuah variabel baru dan datanya misal integer, maka untuk seterusnya data yang bisa hanya integer
+  print(anything);
+}
